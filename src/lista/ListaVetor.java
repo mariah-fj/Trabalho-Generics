6289844eae178ba497fcd079a0d6f1ac0b0e25c6
mariah-fj <mariaheloisafirminojeronimo@gmail.com>
2026-06-21 19:47:06 -0300

    criando a estrutura inicial do projeto

---
+++ src/lista/ListaVetor.java
@@ -0,0 +1,35 @@
+package lista;
+
+public class ListaVetor <T> implements Lista<T>{
+
+
+    @Override
+    public void add(T valor) {
+
+    }
+
+    @Override
+    public void add(T valor, int pos) {
+
+    }
+
+    @Override
+    public T get(int pos) {
+        return null;
+    }
+
+    @Override
+    public T remove(int pos) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(T valor) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
